ba43ddb2677b68903824cb7af1940a99fe9d6c0c
Murray Chen <mikemilk433@gmail.com>
2025-09-22 14:18:51 -0400

    Update myaction_sir.m
    Add the dead parameter

--- myaction_sir.m
+++ myaction_sir.m
@@ -1,10 +1,12 @@
-function [s_n, i_n, r_n] = myaction_sir(s, i, r, beta, gamma)
+function [s_n, i_n, r_n, d_n] = myaction_sir(s, i, r, beta, gamma, mu)
 % compute new infections and recoveries
 infected = beta * i * s;
 recovered = gamma * i;
+dead = mu * i;
     
 % Update state
 s_n = s - infected;
 i_n = i + infected - recovered;
 r_n = r + recovered;
+d_n = d + dead;
 end
